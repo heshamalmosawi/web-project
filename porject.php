755b753a0475ed0823045bbf7bdf9e861e4af1f7
LoayDalal <loaydalal.2003@gmail.com>
2023-12-07 16:56:40 +0300

    123

---
+++ porject.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Radio Buttons Example</title>
+</head>
+<body>
+
+<form>
+  <label>
+    <input type="radio" name="gender" value="male"> Male
+  </label>
+  <label>
+    <input type="radio" name="gender" value="female"> Female
+  </label>
+  <label>
+    <input type="radio" name="gender" value="other"> Other
+  </label>
+
+  <p>Choose your gender</p>
+  <input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
