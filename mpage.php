c126331c361ddaa3c0b7b8f685b88a3756ee9196
Ebraali2003 <ebraali2003@gmail.com>
2023-12-22 00:20:39 +0300

    commen

---
+++ mpage.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css">
+    </head>
+
+     <body>
+        <div class="container">
+            <header></header>
+            <main></main>
+        <div calss="cards">
+            <div class="card1"></div>
+            <div class="card2"></div>
+            <div class="card3"></div>
+        </div>
+
+    
+        </div>
+     </body>
+     
+
+
+    </link>
+</html>
