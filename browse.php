973184b87627b9a787da86173988a3b11f0b3479
LoayDalal <loaydalal.2003@gmail.com>
2023-12-24 22:24:02 +0300

    aaa

--- browse.php
+++ browse.php
@@ -1,5 +1,7 @@
 <?php
 try {
+    session_start();
+
     require('connection.php');
 
     $all_poll = 'SELECT * FROM surveys';
@@ -9,9 +11,14 @@ try {
     $fet = $res->fetchAll(PDO::FETCH_ASSOC);
 
     for ($i = 0; $i < count($fet); ++$i) {
-        echo "<div id=" . $i . " class='allQ' onclick='show(" . $i . ")'> <a href='#'>" . $fet[$i]['question'] . "</a> </div>";
+        $date=json_encode($fet[$i]["expireDate"]);
+        echo "<div id=" . $i . " class='allQ' onclick='show(" . $i . ','. $date. ")'> <a href='#'>" . $fet[$i]['question']. "</a> </div>";
     }
 
+  
+  
+
+    
 
 
     echo "<div id='resultpoll'></div>";
@@ -21,7 +28,7 @@ try {
 ?>
 
 <script>
-    function show(id) {
+    function show(id,s) {
         var resultpoll = document.getElementById("resultpoll");
 
         var a = document.getElementsByClassName("allQ");
@@ -32,12 +39,84 @@ try {
 
         resultpoll.innerHTML = "<div class='poll-container'>" +
             "<div class='poll-info'>Question: " + <?php echo json_encode($fet); ?>[id]["question"] + "</div>" +
-            "<div class='poll-info'>Results: " + <?php echo json_encode($fet); ?>[id]["results"] + "</div>" +
-            "<div class='poll-info'>Voters: " + <?php echo json_encode($fet); ?>[id]["voters"] + "</div>" +
-            "<div class='poll-info'>Expire Date: " + <?php echo json_encode($fet); ?>[id]["expireDate"] + "</div>" +
-            "<div class='poll-info'>Status: " + <?php echo json_encode($fet); ?>[id]["status"] + "</div>" +
+            <?php $result= json_decode($fet); ?>[id]["results"] +
+            "<div class='poll-info'>Results:<?php
+              foreach ($result as $key => $value)
+               {
+                  echo  "<br>".$value."% " ;
+        
+                     echo "<progress  value='$value' max='100'></progress> $key";} ?> </div>"   +
+           
+            "<div class='poll-info ' id='Expire'>Expire Date: " + <?php echo json_encode($fet); ?>[id]["expireDate"] + "</div>" +
+            "<div class='poll-info'>Status: " + <?php echo json_encode($fet); ?>[id]["status"] + "</div>" + 
             "</div>";
+            var expire =document.getElementById("Expire");
+    if (s=="0000-00-00")
+    {
+        expire.style.display="none";
+    }
+
 
     }
 
 </script>
+
+<style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 20px;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+        h2 {
+            color: #007BFF;
+        }
+
+        .poll-container {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        .poll-info {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .button-container {
+            margin-top: 10px;
+        }
+
+        input[type='submit'] {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 8px 16px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type='date'] {
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            margin-right: 10px;
+        }
+
+        a {
+            display: inline-block;
+            margin-top: 10px;
+            color: #007BFF;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+
+       
