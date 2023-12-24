47b90c8f112e6290f78191912a88b9a278816abe
Abdulrahman Jalal <202100690@stu.uob.edu.bh>
2023-12-24 18:57:16 +0300

    added css styling

--- addpoll.php
+++ addpoll.php
@@ -1,187 +1,215 @@
 <?php
-require ("status.php");
-  session_start();
-  echo "Welcome ".$_SESSION['activeuser'];
-  if (!isset($_SESSION['activeuser'])){
-    die("please login!");
-  }
-  if (isset($_POST["create"])){
-    // if(!isset($_POST['close']))  {   echo "<script>alert('chose how to close poll')</script>";}
-    // else{
-    try{
-      $questionName = $_POST["questionName"];
-      $op = $_POST["op"];
-
-      require('connection.php');
-      
-      $rs= $db->prepare("INSERT INTO surveys(question, results, voters, expireDate, creater, status) VALUES(?,?,?,?,?,?)");
-      // this one is question name
-      $rs->bindParam(1, $questionName);
-      
-      // this is to store the options with results in json string, and initialize votes to 0
-      foreach ($op as $key){
-          $results[$key] = 0;
-      }
-      $resultsJ = json_encode($results);
-      $rs->bindParam(2, $resultsJ);
-      
-      // no voters yet
-      $voters = '[]';
-      $rs->bindParam(3, $voters);
-
-      // expire date depending on manual or scedhuled
-      if ($_POST['close'] == 'manual') {
-        $rs->bindValue(4, '');
-      } else {
-        $rs->bindParam(4, $_POST['dateExpiry']);
-      }
-      
-      $rs->bindValue(5, $_SESSION['activeuser']);
-
-      $rs->bindValue(6, 1);
-
-      $rs->execute();
-      
-      #update the users table for the pollsCreated
-      $pollsCreatedStmt = $db->prepare("UPDATE users SET pollsCreated = pollsCreated + 1 WHERE Username = ?");
-      $pollsCreatedStmt->bindValue(1, $_SESSION['activeuser']);
-      $pollsCreatedStmt->execute();
-
-      $db = null;
-      
-      header("Location:userpolls.php"); #for redirecting to addpoll.php
-      exit;
-
-    }catch (PDOException $ex){
-      echo "error: ";
-      die($ex->getMessage());
-      } 
-    
-
-  // }
+require("status.php");
+session_start();
+echo "<div class='o1'>Welcome " . $_SESSION['activeuser'] . "</div>";
+if (!isset($_SESSION['activeuser'])) {
+    die("<div class='o1'>Please login!</div>");
 }
+if (isset($_POST["create"])) {
+    try {
+        $questionName = $_POST["questionName"];
+        $op = $_POST["op"];
 
+        require('connection.php');
 
+        $rs = $db->prepare("INSERT INTO surveys(question, results, voters, expireDate, creater, status) VALUES(?,?,?,?,?,?)");
+        $rs->bindParam(1, $questionName);
+
+        foreach ($op as $key) {
+            $results[$key] = 0;
+        }
+        $resultsJ = json_encode($results);
+        $rs->bindParam(2, $resultsJ);
+
+        $voters = '[]';
+        $rs->bindParam(3, $voters);
+
+        if ($_POST['close'] == 'manual') {
+            $rs->bindValue(4, '');
+        } else {
+            $rs->bindParam(4, $_POST['dateExpiry']);
+        }
+
+        $rs->bindValue(5, $_SESSION['activeuser']);
+        $rs->bindValue(6, 1);
+        $rs->execute();
+
+        $pollsCreatedStmt = $db->prepare("UPDATE users SET pollsCreated = pollsCreated + 1 WHERE Username = ?");
+        $pollsCreatedStmt->bindValue(1, $_SESSION['activeuser']);
+        $pollsCreatedStmt->execute();
+
+        $db = null;
+
+        header("Location:userpolls.php");
+        exit;
+    } catch (PDOException $ex) {
+        echo "<div class='o1'>Error: " . $ex->getMessage() . "</div>";
+    }
+}
 ?>
 
 <head>
-<style>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+            text-align: center;
+        }
 
-body{
-  background-color:grey;
-}
-.o1 {
-  text-align:center;
+        .o1 {
+            width: 250px;
+            border: 10px solid white;
+            border-radius: 10px;
+            margin: 20px auto;
+            background-color: #007BFF;
+            color: white;
+            padding: 10px;
+        }
 
-  width: 250px;
-  border: 10px solid white ;
-  border-radius: 10px;
-}
-.o2{
-  text-align:center;
- 
-  width: 230px;
-  border: 10px solid blue ;
-  border-radius: 10px;
-  margin:-20 0  ;
-}
-  </style>
+        .o2 {
+            width: 230px;
+            border: 10px solid blue;
+            border-radius: 10px;
+            margin: -20 0;
+        }
+
+        form {
+            width: 60%;
+            margin: 20px auto;
+            padding: 20px;
+            border-radius: 10px;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        h1 {
+            color: #007BFF;
+        }
+
+        input[type='text'],
+        input[type='date'] {
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            margin-bottom: 10px;
+        }
+
+        input[type='submit'],
+        input[type='button'] {
+            background-color: #007BFF;
+            color: white;
+            padding: 8px 16px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type='radio'] {
+            margin-right: 5px;
+        }
+
+        p {
+            color: #007BFF;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
-<body>
 
-<form method="POST" onsubmit="return empty()" action="" name="form">
-<h1>make your poll</h1>
-  <label>
-    <input type="text" placeholder="Type your question here" name="questionName">
-  </label>
+<body>
+    <form method="POST" onsubmit="return empty()" action="" name="form">
+        <h1>Make Your Poll</h1>
+        <label>
+            <input type="text" placeholder="Type your question here" name="questionName">
+        </label>
 
         <div id="pollOptions">
-          <label>
-             <input  placeholder="Option 1" type="text" name=op[]>
-          </label>
-          <label>
-           <input placeholder="Option 2" type="text" name=op[]>
-          </label>
+            <label>
+                <input placeholder="Option 1" type="text" name=op[]>
+            </label>
+            <label>
+                <input placeholder="Option 2" type="text" name=op[]>
+            </label>
         </div>
-        
-        <button type="button" onclick="add()">add more option</button>
-        <button type="button" onclick="remove()">remove more option</button>
-          <br>
-            <div>
-              <p>How do you want to close the poll?</p>
-              <input type="radio" id="automatic" name="close" value=automatic onchange="showOrHideDates()">
-              <label for="automatic">By Scheduled date</label>
-              <div id="DateE"></div>
-              <input type="radio" id="manual" value=manual name="close" onchange="showOrHideDates()">
-              <label for="manual">Manual</label>
-            </div>
 
-          <input type="submit"  name="create" id="">
-      </form>
-      
-      <script>
-          var optionCount= 2;
-      function add() {
-        ++optionCount;
-        var pollOptionsContainer = document.getElementById("pollOptions");
-        var newPollOption = document.createElement("label");
-        newPollOption.innerHTML = " <input placeholder='Option "+optionCount+"' name=op[] type='text'>";
+        <button type="button" onclick="add()">Add More Option</button>
+        <button type="button" onclick="remove()">Remove More Option</button>
+        <br>
 
-        pollOptionsContainer.appendChild(newPollOption);
-      }
-      function remove() 
-      {
-        if (optionCount > 2)
-        {
-          var pollOptionsContainer = document.getElementById("pollOptions");
-          pollOptionsContainer.removeChild(pollOptionsContainer.lastChild);
-          --optionCount;
+        <div>
+            <p>How do you want to close the poll?</p>
+            <input type="radio" id="automatic" name="close" value=automatic onchange="showOrHideDates()">
+            <label for="automatic">By Scheduled date</label>
+            <div id="DateE"></div>
+            <input type="radio" id="manual" value=manual name="close" onchange="showOrHideDates()">
+            <label for="manual">Manual</label>
+        </div>
 
-        }else {
-          alert("Atleast two option");
+        <input type="submit" name="create" value="Create Poll">
+    </form>
+
+    <script>
+        var optionCount = 2;
+
+        function add() {
+            ++optionCount;
+            var pollOptionsContainer = document.getElementById("pollOptions");
+            var newPollOption = document.createElement("label");
+            newPollOption.innerHTML = " <input placeholder='Option " + optionCount + "' name=op[] type='text'>";
+
+            pollOptionsContainer.appendChild(newPollOption);
         }
 
-      }
-      function empty()
-      {
-        var questionName = document.forms["form"]["questionName"].value;
-        if (questionName=="" ){
-          alert('Must be write your question');
-          return false;
+        function remove() {
+            if (optionCount > 2) {
+                var pollOptionsContainer = document.getElementById("pollOptions");
+                pollOptionsContainer.removeChild(pollOptionsContainer.lastChild);
+                --optionCount;
+            } else {
+                alert("At least two options are required");
+            }
         }
-        var op =document.getElementsByName("op[]");
-        for (let x in op)
-        {
-          if(op[x].value.trim()=="")
-          alert("answer");
+
+        function empty() {
+            var questionName = document.forms["form"]["questionName"].value;
+            if (questionName == "") {
+                alert('Must write your question');
+                return false;
+            }
+            var op = document.getElementsByName("op[]");
+            for (let x in op) {
+                if (op[x].value.trim() == "") {
+                    alert("Answer cannot be empty");
+                    return false;
+                }
+            }
         }
-      
-      }
-      function zeroPad(number) {
-  return number < 10 ? '0' + number : '' + number;
-}
 
-function showOrHideDates() {
-  var automaticRadio = document.getElementById("automatic");
-  var DateE = document.getElementById("DateE");
+        function zeroPad(number) {
+            return number < 10 ? '0' + number : '' + number;
+        }
 
-  const date = new Date();
+        function showOrHideDates() {
+            var automaticRadio = document.getElementById("automatic");
+            var DateE = document.getElementById("DateE");
 
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
+            const date = new Date();
 
-  let currentDate = `${year}-${month}-${day}`;
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            let year = date.getFullYear();
 
-  if (automaticRadio.checked) {
-    // Show the date input when "By Scheduled date" is selected
-    DateE.innerHTML = '<input name=dateExpiry value="'+currentDate+'" type="date" min="' + currentDate +'">';
-  } else {
-    // Hide the date input for other options
-    DateE.innerHTML = "";
-  }
-}
-</script>
+            let currentDate = `${year}-${zeroPad(month)}-${zeroPad(day)}`;
 
+            if (automaticRadio.checked) {
+                // Show the date input when "By Scheduled date" is selected
+                DateE.innerHTML = '<input name=dateExpiry value="' + currentDate + '" type="date" min="' + currentDate + '">';
+            } else {
+                // Hide the date input for other options
+                DateE.innerHTML = "";
+            }
+        }
+    </script>
 </body>
 </html>
