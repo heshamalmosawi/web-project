401cf823b533a05559e07a7f61fad61ad88c1382
LoayDalal <loaydalal.2003@gmail.com>
2023-12-25 16:15:17 +0300

    aa

--- browse.php
+++ browse.php
@@ -12,23 +12,30 @@ try {
 
     for ($i = 0; $i < count($fet); ++$i) {
         $date=json_encode($fet[$i]["expireDate"]);
-        echo "<div id=" . $i . " class='allQ' onclick='show(" . $i . ','. $date. ")'> <a href='#'>" . $fet[$i]['question']. "</a> </div>";
+        $resjson=json_encode($fet[$i]['results']);
+        $userwhovote= json_encode($fet[$i]['voters']);
+        $isstuts= ($fet[$i]["status"]);
+        echo "<div id=" . $i . " class='allQ' onclick='show(" . $i . ','. $date.','.$resjson.','.$userwhovote.','.$isstuts. ")'> <a href='#'>" . $fet[$i]['question']. "</a> </div> <br>";
+        
     }
 
-    $result =json_decode($fet[0]["results"],true);
-    
+   
+   
+    echo "<div id='tmt'>1</div>";
 
     
 
 
-    echo "<div id='resultpoll'></div>";
+    echo "<div id=''>
+    <form id='resultpoll' method='POST' action=''></form>
+    </div>";
 } catch (PDOException $e) {
     die($e->getMessage());
 }
 ?>
 
 <script>
-    function show(id,s) {
+    function show(id,date,resjson,userwhovote,isstuts) {
         var resultpoll = document.getElementById("resultpoll");
 
         var a = document.getElementsByClassName("allQ");
@@ -36,28 +43,73 @@ try {
             a[i].style.display = "none";
         }
 
-
-        resultpoll.innerHTML = "<div class='poll-container'>" +
+         const r =JSON.parse(resjson);
+         const voters = JSON.parse(userwhovote);
+         var isvote=false;
+       
+         for (let i=0; i<voters.length;++i)
+         {
+            if(voters[i]=='<?php echo $_SESSION['activeuser']; ?>')
+            isvote=true;
+         }
+         if (isstuts==0)
+         {
+            isvote=true;
+         }
+         if (isvote)
+         {
+            resultpoll.innerHTML =
+         "<div class='poll-container'>" +
             "<div class='poll-info'>Question: " + <?php echo json_encode($fet); ?>[id]["question"] + "</div>" +
-            
-            "<div class='poll-info'>Results:<?php
-              foreach ($result as $key => $value)
-               {
-                  echo  "<br>".$value."% " ;
-        
-                     echo "<progress  value='$value' max='100'></progress> $key";} ?> </div>"   +
+            "<div class='poll-info'>Results:";
+           var sumvote=0;
+            for (let key in r)
+            {
+                 sumvote+=r[key];
+            }
+            for (let key in r) {
+                    
+                    resultpoll.innerHTML += key + ' <progress  value="'+r[key] +'"max="'+sumvote+'"></progress> = '+r[key]*100/sumvote + '%<br>';
+                
+                }
+           
+            resultpoll.innerHTML +="</div>"   +
                    
             "<div class='poll-info ' id='Expire'>Expire Date: " + <?php echo json_encode($fet); ?>[id]["expireDate"] + "</div>" +
             "<div class='poll-info'>Status: " + <?php echo json_encode($fet); ?>[id]["status"] + "</div>" + 
             "</div>";
+         }else 
+         {
+                resultpoll.innerHTML ="<div class='poll-container'>" +
+                "<div class='poll-info'>Question: " + <?php echo json_encode($fet); ?>[id]["question"] + "</div>" +
+                "<div class='poll-info'>Results:";
+             
+                for (let key in r) {
+                    
+                        resultpoll.innerHTML += key + "<input type='radio' name='votes' value='"+key+"'>"  + "<br>";
+                    
+                    }
+            
+                resultpoll.innerHTML +="</div> <input type='submit' name='voting' value='vote' onclick=updateSQL("+id+','+resjson+")>"+
+                "<div class='poll-info ' id='Expire'>Expire Date: " + <?php echo json_encode($fet); ?>[id]["expireDate"] + "</div>" +
+                "<div class='poll-info'>Status: " + <?php echo json_encode($fet); ?>[id]["status"] + "</div>" + 
+                "</div>";
+                
+
+         }
             var expire =document.getElementById("Expire");
-    if (s=="0000-00-00")
-    {
-        expire.style.display="none";
-    }
+            if (date=="0000-00-00")
+            {
+                expire.style.display="none";
+            }
+            
+            }
 
-
-    }
+            function updateSQL(id,resjson)
+            {
+                var io =document.getElementsByName("votes").values;
+               document.getElementById("tmt").innerHTML=io;
+            }
 
 </script>
 
@@ -119,4 +171,3 @@ try {
         }
     </style>
 
-       
---
+++ browse2.php
@@ -0,0 +1,172 @@
+<?php
+try {
+    session_start();
+
+    require('connection.php');
+
+    $all_poll = 'SELECT * FROM surveys';
+
+    $res = $db->query($all_poll);
+
+    $fet = $res->fetchAll(PDO::FETCH_ASSOC);
+    echo "<form method='POST' action=''>";
+    for ($i = 0; $i < count($fet); ++$i) {
+   
+        echo "<div id=" . $i . " class='allQ'> <a href='#' name='QP' value='" . $i . "'>" . $fet[$i]['question']. "</a> </div> <br>";
+    }
+    echo "</form>";
+
+    $resjson = json_decode($fet[0]['results'],true);
+    $votjson = json_decode($fet[0]['voters'],true);
+    
+     echo $fet[0]['question']."<br>";
+            $sum=0;
+     foreach ($resjson as $key => $value)
+     {   
+        $sum +=$value;
+     }
+    foreach ($resjson as $key => $value)
+    {   echo $key." ";
+        echo "<progress id='file' value='".$value."' max='".$sum."'></progress> ". $value/$sum . "% <br>";
+    }
+    if ($fet[0]["status"]==1)
+    {
+        echo "Status:Open";   
+    }
+    else 
+    echo "Status:Close";
+
+    echo "<br> who vote in this poll <br>";
+    for ($i=0; $i<count($votjson);++$i)
+    {
+        echo $votjson[$i]."<br>";
+    }
+
+
+    echo "<br><br>";
+    echo "<form method='POST' action=''>";
+    foreach ($resjson as $key => $value)
+    {   echo $key." ";
+        echo "<input type='radio' name='op' value='".$key."'><br>";
+    }
+    echo "<input type='submit' name='voting' value='vote'>";
+    echo "</form>";
+  
+    echo $resjson[$_POST['op']];
+    echo "<br>";
+    if (isset($_POST['voting']))
+    {
+        $resjson[$_POST['op']]++;
+    }
+
+    echo $resjson[$_POST['op']];
+    echo "<br>";
+    $isvote =false;
+    for ($i=0; $i<count($votjson);++$i)
+    {
+        if ($_SESSION['activeuser']==$votjson[$i])
+        {
+            $isvote=true;
+        }
+        
+    }
+   if ($isvote)
+   {
+    echo "no vote";
+   }else
+   echo "vote";
+
+   echo "<br><br>";
+
+
+   
+
+   $updateSQL = $db->prepare("UPDATE surveys (results,voters) VALUES (?,?) WHERE id =?");
+   $upr=json_encode($resjson);
+   $upv=json_encode($votjson);
+   $updateSQL->bindParam(1,$upr);
+   $updateSQL->bindParam(2,$upv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+?>
+<style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 20px;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+        h2 {
+            color: #007BFF;
+        }
+
+        .poll-container {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        .poll-info {
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+
+        .button-container {
+            margin-top: 10px;
+        }
+
+        input[type='submit'] {
+            background-color: #007BFF;
+            color: #fff;
+            padding: 8px 16px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type='date'] {
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            margin-right: 10px;
+        }
+
+        a {
+            display: inline-block;
+            margin-top: 10px;
+            color: #007BFF;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+
